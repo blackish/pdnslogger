ff391365c7037214601adb2d1a5ea8c41ebe3954
Андрей Лысов <blackish@Andrejs-MacBook-Air.local>
2022-10-05 14:52:50 +0300

    multiple messages

--- src/dnslogger/dnslog.go
+++ src/dnslogger/dnslog.go
@@ -59,40 +59,44 @@ func (svc *DNSLogServiceServer) Worker(conn net.Conn) error {
 			slog.Debug("Error reading")
 			break
 		}
-		l = uint(b[1]) + uint(b[0])*256
-		slog.Debug("Read successful")
-		slog.Debugf("Read %d", n)
-		if uint(n) != l+2 {
-			slog.Debugf("Read %d, expect %d", n, l)
-			continue
-		}
-		if err = proto.Unmarshal(b[2:], msg); err != nil {
-			qstring := ""
-			if msg.From != nil && msg.TimeSec != nil && msg.Question != nil {
-				slog.Debugf("Query %s", *msg.Question.QName)
-				qstring = fmt.Sprintf("INSERT INTO %s VALUES (%d,'%s','%s')", svc.Table, time.Now().Unix(), net.IP(msg.From).String(), *msg.Question.QName)
-				err = svc.Ch.AsyncInsert(context.Background(), qstring, false)
-				if err != nil {
-					slog.Debug(err)
-				}
-			} else if msg.To != nil && msg.TimeSec != nil && msg.Response != nil && svc.ResponseTable != "" {
-				slog.Debugf("Response %s", net.IP(msg.To).String())
-				for _, rs := range msg.Response.Rrs {
-					data := ""
-					if *rs.Type == 1 || *rs.Type == 28 {
-						data = net.IP(rs.Rdata).String()
-					} else {
-						data = string(data)
-					}
-					qstring = fmt.Sprintf("INSERT INTO %s VALUES (%d,'%s','%s',%d,'%s')", svc.ResponseTable, time.Now().Unix(), net.IP(msg.To).String(), *rs.Name, *rs.Type, data)
+		npos := 0
+		for npos < uint(n) {
+			l = uint(b[npos+1]) + uint(b[npos])*256
+			slog.Debug("Read successful")
+			slog.Debugf("Read %d", n)
+			if uint(n) < npos+l+2 {
+				slog.Debugf("Read %d, expect %d", n, l)
+				continue
+			}
+			if err = proto.Unmarshal(b[npos+2:npos+2+l], msg); err != nil {
+				qstring := ""
+				if msg.From != nil && msg.TimeSec != nil && msg.Question != nil {
+					slog.Debugf("Query %s", *msg.Question.QName)
+					qstring = fmt.Sprintf("INSERT INTO %s VALUES (%d,'%s','%s')", svc.Table, time.Now().Unix(), net.IP(msg.From).String(), *msg.Question.QName)
 					err = svc.Ch.AsyncInsert(context.Background(), qstring, false)
+					if err != nil {
+						slog.Debug(err)
+					}
+				} else if msg.To != nil && msg.TimeSec != nil && msg.Response != nil && svc.ResponseTable != "" {
+					slog.Debugf("Response %s", net.IP(msg.To).String())
+					for _, rs := range msg.Response.Rrs {
+						data := ""
+						if *rs.Type == 1 || *rs.Type == 28 {
+							data = net.IP(rs.Rdata).String()
+						} else {
+							data = string(data)
+						}
+						qstring = fmt.Sprintf("INSERT INTO %s VALUES (%d,'%s','%s',%d,'%s')", svc.ResponseTable, time.Now().Unix(), net.IP(msg.To).String(), *rs.Name, *rs.Type, data)
+						err = svc.Ch.AsyncInsert(context.Background(), qstring, false)
+					}
+				} else {
+					slog.Debugf("Empty content %d", msg.Type)
 				}
 			} else {
-				slog.Debugf("Empty content %d", msg.Type)
+				slog.Debug("Parse error")
 			}
-		} else {
-			slog.Debug("Parse error")
 		}
+		npos = npos + l + 2
 	}
 	slog.Debug("Worker exited")
 	_ = conn.Close()
